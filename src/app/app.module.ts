0dd2cc6b3b5ec91dabcdbda9f389eece17e35140
Alexkevin1 <Alex.kmatheson@gmail.com>
2018-12-08 18:06:29 -0700

    New-up / clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { GamePlayComponent } from './game-play/game-play.component';
+import { HomeComponent } from './home/home.component';
+import { NewGameComponent } from './home/new-game/new-game.component';
+import { ExsistingGameComponent } from './home/exsisting-game/exsisting-game.component';
+
+const routes: Routes = [
+  { path: 'game-play', component: GamePlayComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'NewGame', component: NewGameComponent},
+  { path: 'ExsistingGame', component: ExsistingGameComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePlayComponent,
+    HomeComponent,
+    NewGameComponent,
+    ExsistingGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/exsisting-game/exsisting-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exsisting-game',
+  templateUrl: './exsisting-game.component.html',
+  styleUrls: ['./exsisting-game.component.css']
+})
+export class ExsistingGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
